d6f8cb892ecc253161d1c05a14d04ee112fc4f26
Brent Ely <gitbrent@gmail.com>
2019-04-05 23:37:09 -0500

    fixed grunt, min files updated for v.1.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,39 @@
-module.exports = function(grunt){
-	'use strict';
+module.exports = function(grunt) {
+  "use strict";
 
-	grunt.initConfig({
-		clean: ['dist'],
-		uglify: {
-			options: {
-				preserveComments: 'some',
-				sourceMap: true
-			},
-			build: {
-				expand: true,
-				cwd: 'js',
-				src: ['**/*.js', ['!**/*.min.js']],
-				dest: 'js',
-				ext: '.min.js',
-			}
-		},
-		cssmin: {
-			options: {
-				keepBreaks: true
-			},
-			build: {
-				expand: true,
-				cwd: 'css',
-				src: ['**/*.css', ['!**/*.min.css']],
-				dest: 'css',
-				ext: '.min.css',
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.registerTask('default', ['clean', 'uglify', 'cssmin']);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    clean: ["dist"],
+    uglify: {
+      options: {
+        preserveComments: "some",
+        sourceMap: true,
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        expand: true,
+        cwd: "js",
+        src: ["**/*.js", ["!**/*.min.js"]],
+        dest: "js",
+        ext: ".min.js"
+      }
+    },
+    cssmin: {
+      options: {
+        keepBreaks: true
+      },
+      build: {
+        expand: true,
+        cwd: "css",
+        src: ["**/*.css", ["!**/*.min.css"]],
+        dest: "css",
+        ext: ".min.css"
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-uglify-es");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.registerTask("default", ["clean", "uglify", "cssmin"]);
 };
