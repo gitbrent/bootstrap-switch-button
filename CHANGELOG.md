fbdf5a93ba413787730ac69486e5dc547b08a27f
Brent Ely <gitbrent@gmail.com>
2020-02-15 23:17:14 -0600

    Fixed Issue #6, Pull #8, Issue #10 (also formatted)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
@@ -6,15 +7,27 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
-## [1.1.0](https://github.com/gitbrent/bootstrap4-toggle/tree/v1.1.0) 2019-??-??
-### [Full Changelog](https://github.com/gitbrent/bootstrap4-toggle/compare/v1.0.0...v1.1.0)
-### Added
-- Added accessibility properties to labels ([aproquot](https://github.com/aproquot))
--- TODO: Add silent option
+## [1.1.0](https://github.com/gitbrent/bootstrap4-toggle/tree/v1.1.0) 2020-02-16
 
-## [1.0.0](https://github.com/gitbrent/bootstrap-switch-button/tree/v1.0.0) 2019-03-05
+### [Full Changelog](https://github.com/gitbrent/bootstrap4-toggle/compare/v1.0.0...v1.1.0)
+
 ### Added
-- Initial release
+
+- Added accessibility properties to labels ([aproquot](https://github.com/aproquot))
+
 ### Changed
 
-[Unreleased]: https://github.com/gitbrent/bootstrap-switch-button/compare/v1.0.0...HEAD
+- Issue #6
+- Pull #8
+- Issue #10
+- TODO: Add silent option to docs
+
+## [1.0.0](https://github.com/gitbrent/bootstrap-switch-button/tree/v1.0.0) 2019-03-05
+
+### Added
+
+- Initial release
+
+### Changed
+
+[unreleased]: https://github.com/gitbrent/bootstrap-switch-button/compare/v1.0.0...HEAD
--- src/bootstrap-switch-button.js
+++ src/bootstrap-switch-button.js
@@ -26,14 +26,14 @@
 |*|  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 |*|  SOFTWARE.
 \*/
-"use strict";
+'use strict';
 
-(function(){
+(function() {
 	/**
-	* `SwitchBtn` is instantiated for each switch-button
-	*/
+	 * `SwitchBtn` is instantiated for each switch-button
+	 */
 	class SwitchBtn {
-		constructor(element,options) {
+		constructor(element, options) {
 			const DEFAULTS = {
 				onlabel: 'On',
 				onstyle: 'primary',
@@ -42,8 +42,8 @@
 				size: '',
 				style: '',
 				width: null,
-				height: null
-			}
+				height: null,
+			};
 			options = options || {};
 
 			// A: Capture ref to HMTL element
@@ -51,14 +51,14 @@
 
 			// B: Set options
 			this.options = {
-				onlabel:  element.getAttribute('data-onlabel')  || options.onlabel || DEFAULTS.onlabel,
-				onstyle:  element.getAttribute('data-onstyle')  || options.onstyle || DEFAULTS.onstyle,
+				onlabel: element.getAttribute('data-onlabel') || options.onlabel || DEFAULTS.onlabel,
+				onstyle: element.getAttribute('data-onstyle') || options.onstyle || DEFAULTS.onstyle,
 				offlabel: element.getAttribute('data-offlabel') || options.offlabel || DEFAULTS.offlabel,
 				offstyle: element.getAttribute('data-offstyle') || options.offstyle || DEFAULTS.offstyle,
-				size:     element.getAttribute('data-size')     || options.size || DEFAULTS.size,
-				style:    element.getAttribute('data-style')    || options.style || DEFAULTS.style,
-				width:    element.getAttribute('data-width')    || options.width || DEFAULTS.width,
-				height:   element.getAttribute('data-height')   || options.height || DEFAULTS.height
+				size: element.getAttribute('data-size') || options.size || DEFAULTS.size,
+				style: element.getAttribute('data-style') || options.style || DEFAULTS.style,
+				width: element.getAttribute('data-width') || options.width || DEFAULTS.width,
+				height: element.getAttribute('data-height') || options.height || DEFAULTS.height,
 			};
 
 			// LAST: Render switchButton
@@ -73,24 +73,24 @@
 				const paddingTop = parseFloat(styles.paddingTop);
 				const paddingBottom = parseFloat(styles.paddingBottom);
 
-				return (height - borderBottomWidth - borderTopWidth - paddingTop - paddingBottom);
+				return height - borderBottomWidth - borderTopWidth - paddingTop - paddingBottom;
 			}
 
 			// 1: On
 			var switchOn = document.createElement('label');
-			switchOn.setAttribute('class', 'btn btn-'+this.options.onstyle+' btn-'+this.options.size);
-			switchOn.setAttribute('for', this.element.id)
+			switchOn.setAttribute('class', 'btn btn-' + this.options.onstyle + ' btn-' + this.options.size);
+			switchOn.setAttribute('for', this.element.id);
 			switchOn.innerHTML = this.options.onlabel;
 
 			// 2: Off
 			var switchOff = document.createElement('label');
-			switchOff.setAttribute('class', 'btn btn-'+this.options.offstyle+' btn-'+this.options.size);
-			switchOn.setAttribute('for', this.element.id)
+			switchOff.setAttribute('class', 'btn btn-' + this.options.offstyle + ' btn-' + this.options.size);
+			switchOn.setAttribute('for', this.element.id);
 			switchOff.innerHTML = this.options.offlabel;
 
 			// 3: Handle
 			var switchHandle = document.createElement('span');
-			switchHandle.setAttribute('class', 'switch-handle btn btn-light btn-'+this.options.size);
+			switchHandle.setAttribute('class', 'switch-handle btn btn-light btn-' + this.options.size);
 
 			// 4: Button-Group
 			var switchGroup = document.createElement('div');
@@ -102,31 +102,33 @@
 			// 5: Container
 			var switchCont = document.createElement('div');
 			switchCont.setAttribute('class', 'switch btn');
-			switchCont.classList.add( this.element.checked ? 'btn-'+this.options.onstyle : 'btn-'+this.options.offstyle );
-			if ( this.options.size  ) switchCont.classList.add( 'btn-'+this.options.size );
-			if ( this.options.style ) switchCont.classList.add( this.options.style );
+			switchCont.classList.add(this.element.checked ? 'btn-' + this.options.onstyle : 'btn-' + this.options.offstyle);
+			if (this.options.size) switchCont.classList.add('btn-' + this.options.size);
+			if (this.options.style) switchCont.classList.add(this.options.style);
 
 			// 6: Replace HTML checkbox with Switch-Button
 			this.element.parentElement.insertBefore(switchCont, this.element);
 			//this.element.style.display = 'none';
 			switchCont.appendChild(this.element);
-		    switchCont.appendChild(switchGroup);
+			switchCont.appendChild(switchGroup);
 
 			// 7: Set button W/H, lineHeight
 			{
 				// A: Set style W/H
 				// NOTE: `offsetWidth` returns *rounded* integer values, so use `getBoundingClientRect` instead.
-				switchCont.style.width  = ( this.options.width  || Math.max(switchOn.getBoundingClientRect().width,  switchOff.getBoundingClientRect().width+(switchHandle.getBoundingClientRect().width/2)) )+'px';
-				switchCont.style.height = ( this.options.height || Math.max(switchOn.getBoundingClientRect().height, switchOff.getBoundingClientRect().height) )+'px';
+				switchCont.style.width =
+					(this.options.width ||
+						Math.max(switchOn.getBoundingClientRect().width, switchOff.getBoundingClientRect().width + switchHandle.getBoundingClientRect().width / 2)) + 'px';
+				switchCont.style.height = (this.options.height || Math.max(switchOn.getBoundingClientRect().height, switchOff.getBoundingClientRect().height)) + 'px';
 
 				// B: Apply on/off class
 				switchOn.classList.add('switch-on');
 				switchOff.classList.add('switch-off');
 
 				// C: Finally, set lineHeight if needed
-				if ( this.options.height ) {
-					switchOn.style.lineHeight = calcH(switchOn)+'px';
-					switchOff.style.lineHeight = calcH(switchOff)+'px';
+				if (this.options.height) {
+					switchOn.style.lineHeight = calcH(switchOn) + 'px';
+					switchOff.style.lineHeight = calcH(switchOff) + 'px';
 				}
 			}
 
@@ -142,39 +144,34 @@
 
 			// 11: Fire events
 			this.update(true);
-			this.trigger(true);
-
-			// 12: Add event listener so changes to `checked` property on the HMTL element can be handled
-			// NOTE: NOPE: Hidden elements wont fire events (even if listener is added before element is hidden)
-			//this.element.addEventListener("change", this.update(true));
-			// Users will have to call methods: `toggle`, `on`, `off`, to change state, not use checked property of #ID
+			//this.trigger(true); // 20200215: TODO: theres an Issue on a diff version of the lib where triggering change on load isnt a good thing
 		}
 
 		toggle(event) {
-			if ( this.element.checked ) this.off();
+			if (this.element.checked) this.off();
 			else this.on();
-			if ( typeof event !== 'undefined' && event ) event.preventDefault();
+			if (typeof event !== 'undefined' && event) event.preventDefault();
 		}
 		on(silent) {
-			if ( this.element.disabled ) return false;
-			this.switch.classList.remove( 'btn-'+this.options.offstyle );
-			this.switch.classList.add( 'btn-'+this.options.onstyle );
-			this.switch.classList.remove( 'off' );
+			if (this.element.disabled) return false;
+			this.switch.classList.remove('btn-' + this.options.offstyle);
+			this.switch.classList.add('btn-' + this.options.onstyle);
+			this.switch.classList.remove('off');
 			this.element.checked = true;
-			if ( !silent ) this.trigger();
+			if (!silent) this.trigger();
 		}
 		off(silent) {
-			if ( this.element.disabled ) return false;
-			this.switch.classList.remove( 'btn-'+this.options.onstyle );
-			this.switch.classList.add( 'btn-'+this.options.offstyle );
-			this.switch.classList.add( 'off' );
+			if (this.element.disabled) return false;
+			this.switch.classList.remove('btn-' + this.options.onstyle);
+			this.switch.classList.add('btn-' + this.options.offstyle);
+			this.switch.classList.add('off');
 			this.element.checked = false;
-			if ( !silent ) this.trigger();
+			if (!silent) this.trigger();
 		}
 
 		/**
-		* Keep Switch-Button and corresponding checkbox in-sync
-		*/
+		 * Keep Switch-Button and corresponding checkbox in-sync
+		 */
 		enable() {
 			this.switch.classList.remove('disabled');
 			this.switch.removeAttribute('disabled');
@@ -193,12 +190,8 @@
 			else this.off(silent);
 		}
 		trigger(silent) {
-			// TODO:
-			//switchBtn.off('change.bs.switch');
-			//if ( !silent ) this.element.change();
-			//this.element.on('change.bs.switch', $.proxy(function() {
-			//	this.update()
-			//}, this))
+			if (!silent) this.element.dispatchEvent(new Event('change', { bubbles: true }));
+			//this.update(silent);
 		}
 		destroy() {
 			// A: Remove button-group from UI, replace checkbox element
@@ -212,37 +205,36 @@
 	}
 
 	/**
-	* Add `switchButton` prototype function to HTML Elements
-	* Enables execution when used with HTML - ex: `document.getElementById('switch-trigger').switchButton('on')`
-	*/
+	 * Add `switchButton` prototype function to HTML Elements
+	 * Enables execution when used with HTML - ex: `document.getElementById('switch-trigger').switchButton('on')`
+	 */
 	Element.prototype.switchButton = function(options) {
-		var _switchBtn = ( this.switchBtn || new SwitchBtn(this,options) );
+		var _switchBtn = this.switchBtn || new SwitchBtn(this, options);
 
 		// Execute method calls
-		if ( options && typeof options === 'string' ) {
-			if ( options.toLowerCase() == "toggle" ) _switchBtn.toggle();
-			else if ( options.toLowerCase() == "on" ) _switchBtn.on(false);
-			else if ( options.toLowerCase() == "off" ) _switchBtn.off(false);
-			else if ( options.toLowerCase() == "enable" ) _switchBtn.enable();
-			else if ( options.toLowerCase() == "disable" ) _switchBtn.disable();
-			else if ( options.toLowerCase() == "destroy" ) _switchBtn.destroy();
+		if (options && typeof options === 'string') {
+			if (options.toLowerCase() == 'toggle') _switchBtn.toggle();
+			else if (options.toLowerCase() == 'on') _switchBtn.on(false);
+			else if (options.toLowerCase() == 'off') _switchBtn.off(false);
+			else if (options.toLowerCase() == 'enable') _switchBtn.enable();
+			else if (options.toLowerCase() == 'disable') _switchBtn.disable();
+			else if (options.toLowerCase() == 'destroy') _switchBtn.destroy();
 		}
-	}
+	};
 
 	/**
-	* Replace all `.switch-button` inputs with "Bootstrap-Switch-Button"
-	* Executes once page elements have rendered enabling script to be placed in `<head>`
-	*/
-	if ( typeof window !== 'undefined' ) window.onload = function(){
-		document
-		.querySelectorAll('input[type=checkbox][data-toggle="switchbutton"]')
-		.forEach(function(ele){
-			ele.switchButton();
-		});
+	 * Replace all `.switch-button` inputs with "Bootstrap-Switch-Button"
+	 * Executes once page elements have rendered enabling script to be placed in `<head>`
+	 */
+	if (typeof window !== 'undefined')
+		window.onload = function() {
+			document.querySelectorAll('input[type=checkbox][data-toggle="switchbutton"]').forEach(function(ele) {
+				ele.switchButton();
+			});
+		};
+
+	// Export library if possible
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = SwitchBtn;
 	}
 })();
-
-// Export library if possible
-if ( typeof module !== 'undefined' && module.exports ) {
-	module.exports = SwitchBtn;
-}
